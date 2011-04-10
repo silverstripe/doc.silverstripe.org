df17a5d708192430b91cebefa64792a12809a785
Ingo Schommer <ingo@silverstripe.com>
2011-04-10 21:57:00 +0000

    BUGFIX Allowing PHPUnit 3.4 compat with sapphiredocs
    git-svn-id: http://svn.silverstripe.com/projects/ss2doc/branches/v2@117897 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- sapphiredocs/tests/DocumentationViewerTest.php
+++ sapphiredocs/tests/DocumentationViewerTest.php
@@ -99,7 +99,7 @@ class DocumentationViewerTest extends FunctionalTest {
 		$this->assertFalse($child1->Children);
 		$child2 = $pagesArr[2];
 		
-		$this->assertInstanceOf('DataObjectSet', $child2->Children);
+		$this->assertType('DataObjectSet', $child2->Children);
 	
 		$this->assertEquals(
 			array('subfolder/subpage.md', 'subfolder/subsubfolder/'),
