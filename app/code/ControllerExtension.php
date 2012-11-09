765098aad6b954e070c2afd7db28d7607176305c
Will Rossiter <will.rossiter@gmail.com>
2012-11-10 11:20:03 +1300

    FIX: remove ssorgtheme, use all styles from docviewer

--- app/code/ControllerExtension.php
+++ app/code/ControllerExtension.php
@@ -2,32 +2,22 @@
 
 class ControllerExtension extends Extension {
 	
-	static $google_analytics_code = null;
-
-	public function onBeforeInit() {
-
-		Requirements::combine_files('doc.css', array(
-			'themes/ssorgsites/css/core.css',
-			'themes/ssorgsites/css/grid.css',
-			'themes/ssorgsites/css/typography.css',
-			'themes/ssorgsites/css/form.css',
-			'docsviewer/css/DocumentationViewer.css'
-		));
-
-		Requirements::css('themes/ssorgsites/css/print.css', 'print');
-	}
-	
 	/**
-	 * @return String
+	 * @var string
 	 */
-	function getGoogleAnalyticsCode() {
+	public static $google_analytics_code = null;
+
+	/**
+	 * @return string 
+	 */
+	public function getGoogleAnalyticsCode() {
 		return self::$google_analytics_code;
 	}
 	
 	/**
-	 * @return Bool
+	 * @return boolean
 	 */
-	function IsDev() {
+	public function IsDev() {
 		return (Director::isDev());
 	}
 }
