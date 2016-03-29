7218deb12629659a427fce9fd7392e4ac4719c43
David Alexander <david@spiritlevel.nz>
2016-03-28 19:01:55 -0600

    switched to svn export command for copying documentation files
    whitespace
    
    update README to include need for subversion
    
    editied links and wording
    
    editied wording
    
    editied wording
    
    editied wording
    
    editied wording
    
    editied wording
    
    update README and adjust folder permissions in task
    
    whitespace

--- app/code/RefreshMarkdownTask.php
+++ app/code/RefreshMarkdownTask.php
@@ -36,11 +36,14 @@ class RefreshMarkdownTask extends BuildTask
     public function run($request)
     {
         $this->request = $request;
-        $this->printLine("Refreshing markdown files...");
         $repositories = $this->getRepositories();
         foreach ($repositories as $repository) {
             $this->cloneRepository($repository);
         }
+        $this->printLine(" ");
+        $this->printLine("To re-index the freshly downloaded documentation files either:");
+        $this->printLine("(1) run the command 'sake dev/tasks/RebuildLuceneDocsIndex flush=1' in a shell or");
+        $this->printLine("(2) point your browser at the url 'http://localhost/path/to/ssdocs/dev/tasks/RebuildLuceneDocsIndex?flush=1'");
     }
 
     /**
@@ -92,28 +95,20 @@ class RefreshMarkdownTask extends BuildTask
         $path = $this->getPath();
 
         exec("mkdir -p {$path}/src");
+        exec("chmod -R 775 {$path}/src" );
         exec("rm -rf {$path}/src/{$folder}_{$branch}");
         chdir("{$path}/src");
 
         // If the dev=1 flag is used when RefreshMarkdownTask is run, a full git clone of the framework repository is kept
         // to enable local development of framework and doc.silverstripe.org from within doc.silverstripe.org. Otherwise,
-        // only a shallow clone of the framework repository is made and all non-markdown files deleted, only allowing viewing
-        // of documentation files. Note: The --depth 1 option in the git clone command implies the option --single-branch
+        // only the documentation source files are downloaded, only allowing the viewing of documentation files. 
 
         if( $this->request->getVar('dev') ) {
-            $this->printLine("Performing full clone of " . $remote . "/" . $branch);
-            exec("git clone -q https://github.com/{$remote}.git {$folder}_{$branch} --branch {$branch}");
+            $this->printLine("Cloning repository {$remote}/{$branch} into assets/src/{$folder}_{$branch}");
+            exec("git clone --quiet https://github.com/{$remote}.git {$folder}_{$branch} --branch {$branch}");
         } else {
-            $this->printLine("Performing shallow clone of " . $remote . "/" . $branch);
-            exec("git clone -q https://github.com/{$remote}.git {$folder}_{$branch} --branch {$branch} --depth 1");
-            $this->printLine("Deleting non-documentation framework files from shallow clone of " . $remote . "/" . $branch);
-            chdir("{$path}/src/{$folder}_{$branch}");
-            $framework_paths = array_merge(glob("*"), glob(".*"));
-            foreach ($framework_paths as $framework_path) {
-                if ( $framework_path !== "docs" && $framework_path !=="." && $framework_path !==".." ) {
-                    exec("rm -rf {$framework_path}");
-                }
-            }
+            $this->printLine("Downloading the latest documentation files from repository {$remote}/{$branch} to assets/src/{$folder}_{$branch}");
+            exec("svn export --quiet https://github.com/{$remote}.git/branches/{$branch}/docs {$folder}_{$branch}/docs");
         }
 
     }
