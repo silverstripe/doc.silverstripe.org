87cbb04d9bcb578a19abe593992fdd06add1ccc9
Aaron Carlino <unclecheese@leftandmain.com>
2020-02-12 16:32:55 +1300

    Fix api tag parsing

--- src/utils/cleanApiTags.ts
+++ src/utils/cleanApiTags.ts
@@ -2,10 +2,11 @@
  * Completes the shorthand [api:Something] syntax to [api:Something](Something)
  * @param html
  */
-const cleanApiTags = (html: string): string => {
-    return html.replace(
+const cleanApiTags = (html: string): string => {    
+    return html
+    .replace(
         /\[api:(.*?)\]([^(])/g,
-        (_, query, next) => `<a href="${query}">${query}</a>${next}`
+        (_, query, next) => `<a href="api:${query}">${query}</a>${next}`
     )
 };
 
