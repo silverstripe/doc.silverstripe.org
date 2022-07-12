a7426b66c49e4b66b044ded144db21077e34b42e
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2022-07-12 12:45:06 +1200

    MNT Consume dev docs from the new developer-docs repo (#243)

--- sources-docs.js
+++ sources-docs.js
@@ -3,18 +3,18 @@ module.exports = [
         resolve: `gatsby-source-git`,
         options: {
           name: `docs--4`,
-          remote: `https://github.com/silverstripe/silverstripe-framework.git`,
-          branch: `4`,
-          patterns: `docs/en/**`
+          remote: `https://github.com/silverstripe/developer-docs.git`,
+          branch: `4.11`,
+          patterns: `en/**`
         }
       },    
       {
         resolve: `gatsby-source-git`,
         options: {
           name: `docs--3`,
-          remote: `https://github.com/silverstripe/silverstripe-framework.git`,
+          remote: `https://github.com/silverstripe/developer-docs.git`,
           branch: `3`,
-          patterns: `docs/en/**`
+          patterns: `en/**`
         }
       },    
 ];
