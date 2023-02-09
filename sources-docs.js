209180182cca8955a613806af13b6e01865a3368
Steve Boyd <emteknetnz@gmail.com>
2023-02-09 16:41:17 +1300

    MNT Update v5 docs branch to 5.0
    Also convert backtick to single quote in docs src, as this seems to fix
    the problem where the 5.0 branch wasn't being picked up as a valid
    source.

--- sources-docs.js
+++ sources-docs.js
@@ -1,29 +1,29 @@
 module.exports = [
   {
-    resolve: `gatsby-source-git`,
+    resolve: 'gatsby-source-git',
     options: {
-      name: `docs--5`,
-      remote: `https://github.com/silverstripe/developer-docs.git`,
-      branch: `5`,
-      patterns: `en/**`
+      name: 'docs--5',
+      remote: 'https://github.com/silverstripe/developer-docs.git',
+      branch: '5.0',
+      patterns: 'en/**'
     }
   },
   {
-    resolve: `gatsby-source-git`,
+    resolve: 'gatsby-source-git',
     options: {
-      name: `docs--4`,
-      remote: `https://github.com/silverstripe/developer-docs.git`,
-      branch: `4.13`,
-      patterns: `en/**`
+      name: 'docs--4',
+      remote: 'https://github.com/silverstripe/developer-docs.git',
+      branch: '4.13',
+      patterns: 'en/**'
     }
   },
   {
-    resolve: `gatsby-source-git`,
+    resolve: 'gatsby-source-git',
     options: {
-      name: `docs--3`,
-      remote: `https://github.com/silverstripe/developer-docs.git`,
-      branch: `3`,
-      patterns: `en/**`
+      name: 'docs--3',
+      remote: 'https://github.com/silverstripe/developer-docs.git',
+      branch: '3',
+      patterns: 'en/**'
     }
   },
 ];
