f390d10a3d967bf767feba416a14016d3f384dea
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-10-18 11:56:32 +1300

    FIX Render callout blocks correctly

--- src/utils/parseCalloutTags.ts
+++ src/utils/parseCalloutTags.ts
@@ -5,19 +5,8 @@ import CalloutBlock from '../components/CalloutBlock';
  * Turn [hint] and other callouts into a proper React component.
  * @param data
  */
-const parseCalloutTags = (data: any): ReactElement|false => {
-    let matches;
-
-    matches = data.match(/\[(hint|warning|info|alert|notice|note)\](.*?)\[\/(?:hint|warning|info|alert|notice|note)\]/s);
-
-    if (matches) {
-        const type = matches[1];
-        const content = matches[2];
-
-        return createElement(CalloutBlock, { type, content });
-    }
-
-    return false;
+const parseCalloutTags = (type: string, content: any): ReactElement|false => {
+    return createElement(CalloutBlock, { type, content });
 };
 
 export default parseCalloutTags;
