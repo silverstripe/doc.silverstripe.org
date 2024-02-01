7dd05e61c5da71770c967af184eba575f7c2b9a4
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-02-02 12:23:33 +1300

    FIX correctly parse links inside callout blocks

--- src/utils/parseCalloutTags.ts
+++ src/utils/parseCalloutTags.ts
@@ -1,12 +1,16 @@
 import { ReactElement, createElement } from 'react';
+import { DomElement, HTMLReactParserOptions, domToReact } from "html-react-parser";
 import CalloutBlock from '../components/CalloutBlock';
 
 /**
  * Turn [hint] and other callouts into a proper React component.
  * @param data
  */
-const parseCalloutTags = (type: string, content: any): ReactElement|false => {
-    return createElement(CalloutBlock, { type, content });
+const parseCalloutTags = (type: string, domChildren: DomElement[], parseOptions: HTMLReactParserOptions): ReactElement|false => {
+    return createElement(CalloutBlock, {
+        type,
+        content: domToReact(domChildren, parseOptions),
+    });
 };
 
 export default parseCalloutTags;
