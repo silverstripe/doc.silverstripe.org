53e061db0c9837040b5bcf028b38d7b681badb89
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-02-15 17:54:53 +1300

    FEAT Add linkfield docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,7 +48,7 @@ exports.onCreateNode = async ({ node, getNode, getNodesByType, actions, createNo
     }
     return;
   }   
-  const basePath = category === 'user' ? `docs/en/userguide` : `en`;
+  const basePath = category === 'user' ? 'docs/en/userguide' : (thirdparty ? 'docs/en' : 'en');
   const filePath = createFilePath({
     node,
     getNode,
--- sources-docs.js
+++ sources-docs.js
@@ -11,6 +11,15 @@ module.exports = [
   {
     resolve: 'gatsby-source-git',
     options: {
+      name: 'docs--5--optional_features/linkfield',
+      remote: 'https://github.com/silverstripe/silverstripe-linkfield.git',
+      branch: '4.0',
+      patterns: 'docs/en/**'
+    }
+  },
+  {
+    resolve: 'gatsby-source-git',
+    options: {
       name: 'docs--4',
       remote: 'https://github.com/silverstripe/developer-docs.git',
       branch: '4.13',
