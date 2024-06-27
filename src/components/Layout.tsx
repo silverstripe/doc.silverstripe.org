3f7fd1f386fa44721798753877825f17a8dc0624
Steve Boyd <emteknetnz@gmail.com>
2024-06-27 19:28:12 +1200

    ENH Link github icon to current repo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import useHierarchy from '../hooks/useHierarchy';
 import Helmet from 'react-helmet';
+import LayoutContext from '../contexts/LayoutContext';
 
 interface LayoutProps {
   children?: ReactNode
@@ -13,6 +14,7 @@ interface LayoutProps {
 const Layout: StatelessComponent<LayoutProps> = ({ children, pageContext: { slug } }) => {
   const { setCurrentPath, getVersionPath, getCurrentVersion, getCurrentNode, getDefaultVersion, getVersionMessage } = useHierarchy();
   const [isToggled, setSidebarOpen] = useState(false);
+  const [currentGitRemote, setCurrentGitRemote] = useState(null);
   const handleNavigate = () => setSidebarOpen(false);
   
   setCurrentPath(slug);
@@ -32,24 +34,29 @@ const Layout: StatelessComponent<LayoutProps> = ({ children, pageContext: { slug
     });
   }
 
+  const value = {
+    currentGitRemote,
+    setCurrentGitRemote,
+  };
+
   return (
-    <>
-    <Helmet link={helmetLinks} />
-    <Header handleSidebarToggle={() => setSidebarOpen(!isToggled)} />
-    <div className={`docs-wrapper container ${isToggled ? 'sidebar-visible' : ''}`}>
-    <Sidebar onNavigate={handleNavigate} isOpen={isToggled} />
-      <div className="docs-content">
-        <div className="container">
-          <article role="main" className="docs-article">
-            <>
-            {versionMessage}
-            {children}
-            </>
-          </article>
-        </div> 
+    <LayoutContext.Provider value={value}>
+      <Helmet link={helmetLinks} />
+      <Header handleSidebarToggle={() => setSidebarOpen(!isToggled)} />
+      <div className={`docs-wrapper container ${isToggled ? 'sidebar-visible' : ''}`}>
+      <Sidebar onNavigate={handleNavigate} isOpen={isToggled} />
+        <div className="docs-content">
+          <div className="container">
+            <article role="main" className="docs-article">
+              <>
+              {versionMessage}
+              {children}
+              </>
+            </article>
+          </div>
+        </div>
       </div>
-    </div>
-    </>
+    </LayoutContext.Provider>
   );
 };
 export default Layout;
---
+++ src/contexts/LayoutContext.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LayoutContext = React.createContext(null);
+
+export default LayoutContext;
